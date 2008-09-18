0e170b83c2d78dbd075b1db21be53c73a9498433
Authors <psmpackage@imm.dtu.dk>
2008-09-18 00:00:00 +0000

    version 0.8-3

--- R/PSM.plot.R
+++ R/PSM.plot.R
@@ -7,7 +7,8 @@ PSM.plot <- function(Data, Smooth = NULL, indiv = NULL, type = NULL) {
   if(!is.null(Smooth)) {
     issmooth <- length(Smooth)
     for(i in 1:length(Smooth))
-      issmooth <- issmooth && all(c("Time","Xs","Ps","Xf","Pf","Xp","Pp","Yp","R")
+      issmooth <- issmooth && all(c("Time","Xs","Ps","Xf","Pf",
+                                    "Xp","Pp","Yp","R")
                                   %in% names(Smooth[[i]]))
     if(!issmooth)
       stop('Smooth does not appear to be valid.')
@@ -50,12 +51,22 @@ PSM.plot <- function(Data, Smooth = NULL, indiv = NULL, type = NULL) {
         title(main=paste('Subject',j))
     }
   }
-  dataYplot <- function(log) {
-    for(i in 1:dimY) {
-      plot(Data[[j]][['Time']], Data[[j]][['Y']][i,],
-           xlab="",ylab="",log=log)
+  dataplot <- function(log, type) { #plots Y and U
+    if(is.null(Data[[j]][[type]]))
+      stop(paste('Data does not contain ',type,sep=""))
+    len <- dim(Data[[j]][[type]])[1]
+    if(type=='Y') {
+      len <- dimY
+      plottype <- 'p'
+    } else {
+      len <- dimU
+      plottype <- 's' #show zero-order hold on input.
+    }
+    for(i in 1:len) {
+      plot(Data[[j]][['Time']], Data[[j]][[type]][i,], xlab="",
+           ylab="", log=log, type=plottype)
       if(j==indiv[1])
-        mtext(paste('Y',i,sep=""),side=2,line=2.5)
+        mtext(paste(type,i,sep=""),side=2,line=2.5)
       if(i==1 && k==1)
         title(main=paste('Subject',j))
     }
@@ -122,15 +133,20 @@ PSM.plot <- function(Data, Smooth = NULL, indiv = NULL, type = NULL) {
   } else {
     dimX <- dim(Smooth[[1]]$Xs)[1]
   }
-  dimY <- ifelse(is.null(Data),0,dim(Data[[1]]$Y)[1])
+  dimY <- dim(Data[[1]]$Y)[1]
+  dimU <- ifelse(is.null(Data[[1]]$U),0,dim(Data[[1]]$U)[1])
   if(is.null(indiv))
     indiv = 1:dimS
   numrows <- 0
   for(k in 1:length(type) )
     if('X' %in% unlist(strsplit(type[k],''))) {
       numrows <- numrows + dimX
-    } else  {
-      numrows <- numrows + dimY
+    } else {
+      if ('U' %in% unlist(strsplit(type[k],''))) {
+        numrows <- numrows + dimU
+      } else {
+        numrows <- numrows + dimY
+      }
     }
   #Setup plot
   par(mfcol=c(numrows,length(indiv)),mar=c(2,4,2,0)+.1)
@@ -156,7 +172,8 @@ PSM.plot <- function(Data, Smooth = NULL, indiv = NULL, type = NULL) {
              Ys.Y = smoothplot('Ys',obs=TRUE,len=dimY,log=log),
              X = dataXplot('',log=log),
              longX = dataXplot('long',log=log),
-             Y = dataYplot(log=log),
+             Y = dataplot(log=log,type=tk),
+             U = dataplot(log=log,type=tk),
              res = resplot(log=log),
              acf = acfplot(),
              eta = etaplot(),
